2fbc62bbc387be97ae02a41406ad175ed1d0ebc0
nadia827 <nadia.matias.827@gmail.com>
2016-10-14 18:26:37 -0300

    create areatrapezoide.py

---
+++ areatrapezoide.py
@@ -0,0 +1,13 @@
+print("cálculo del área de un trapezoide")
+
+#datos necesarios para calcular
+
+altura_trapezoide = float(input("si la altura es:"))
+base_inferior = float(input("la base inferior es:"))
+base_superior = float(input("la base superior es:"))
+
+
+area = 0.5*(base_inferior + base_superior)*altura_trapezoide
+
+print("entonces, el área del bendito trapezoide es de", area, "cm^2")
+
